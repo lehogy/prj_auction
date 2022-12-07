a4cec645e4c6a86d2e048612e8bfa3d3ede79fa6
lehogy <m16_a1@naver.com>
2022-12-07 20:05:45 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,157 @@
+from flask import Flask, session, render_template, redirect, request, url_for, jsonify, escape
+
+import pymysql, hashlib, ctypes
+
+# DB 연결
+db = pymysql.connect(host="localhost", user="root", password="nkhg3395!", charset="utf8")  # 기본 양식
+cursor = db.cursor(pymysql.cursors.DictCursor)  # 테이블명까지 보여줌
+cursor.execute('USE auction')  # 이 데이터베이스 사용할 것이다.
+
+app = Flask(__name__)
+app.secret_key = 'ABCDEF'
+
+
+# @app.route('/')
+# def home():
+#     return render_template('login_page.html')
+
+
+# 로그인
+@app.route('/', methods=['GET', 'POST'])
+def login_proc():
+    if request.method == 'POST':
+        userId = request.form['idIn']
+        userPw = request.form['pwIn']
+
+        if len(userId) == 0 or len(userPw) == 0:
+            return 'ID, PW를 입력하세요.'
+        else:
+            sql = "SELECT * FROM sign_up"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+                if userId == row['user_id'] and hashlib.sha256(userPw.encode()).hexdigest() == row['password']:
+                    session['userId'] = userId
+                    return redirect(url_for('main'))
+                # else:
+                #     ctypes.windll.user32.MessageBoxW(0, "아이디 또는 비밀번호가 틀렸습니다.", "알림", 0)
+                #     return redirect(url_for('login_proc'))
+
+    return render_template('login_page.html')
+
+
+# 로그아웃
+@app.route('/logout')
+def logout():
+    session.pop('userId', None)
+    return redirect(url_for('login_proc'))
+
+
+# 회원가입
+@app.route('/modal.html/register', methods=['GET', 'POST'])
+def modal():
+    if request.method == 'POST':
+        result = request.form
+        myid = request.form['Id']
+        name = request.form['Name']
+        pw = request.form['psw']
+        pwr = request.form['psw-repeat']
+
+        if pw != pwr:
+            ctypes.windll.user32.MessageBoxW(0, "비밀번호를 제대로 확인하세요.", "알림", 0)
+            return redirect(url_for('modal'))
+        else:
+            sql = """
+            INSERT INTO sign_up VALUES(
+            NULL,
+            '""" + str(myid) + """',
+            '""" + str(name) + """',
+            '""" + hashlib.sha256(pw.encode()).hexdigest() + """');"""
+
+            cursor.execute(sql)
+
+            cursor.execute('SELECT * FROM sign_up')
+            value = cursor.fetchall()
+
+            print(value)
+
+            db.commit()
+
+            print("END")
+
+        return redirect(url_for('login_proc'))
+
+    return render_template('modal.html')
+
+
+# 메인페이지
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    if "userId" in session:
+        return render_template('main_page.html')
+    else:
+        return jsonify("Who are you?")
+
+
+@app.route('/main/feed', methods=['GET'])
+def feed_box_get():
+    sql = "SELECT * FROM feed"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return jsonify({'feeds': data})
+
+
+# IT 페이지
+@app.route('/it', methods=['GET', 'POST'])
+def it():
+    if "userId" in session:
+        return render_template('main_page.html')
+    else:
+        return jsonify("Who are you?")
+
+
+@app.route('/it/feed', methods=['GET'])
+def feed_it_box_get():
+    sql = "SELECT * FROM feed WHERE category = 'it'"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return jsonify({'it': data})
+
+
+# electron 페이지
+@app.route('/electron', methods=['GET', 'POST'])
+def electron():
+    if "userId" in session:
+        return render_template('electron_page.html')
+    else:
+        return jsonify("Who are you?")
+
+
+@app.route('/electron/feed', methods=['GET'])
+def feed_electron_box_get():
+    sql = "SELECT * FROM feed WHERE category = 'electron'"
+    cursor.execute(sql)
+    data2 = cursor.fetchall()
+    return jsonify({'electron': data2})
+
+
+# life 페이지
+@app.route('/life', methods=['GET', 'POST'])
+def life():
+    if "userId" in session:
+        return render_template('life_page.html')
+    else:
+        return jsonify("Who are you?")
+
+
+@app.route('/life/feed', methods=['GET'])
+def feed_life_box_get():
+    sql = "SELECT * FROM feed WHERE category = 'life'"
+    cursor.execute(sql)
+    data3 = cursor.fetchall()
+    return jsonify({'life': data3})
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000', debug=True)
---
+++ static/main_page.js
@@ -0,0 +1,59 @@
+$(document).ready(function (){
+    feed_box()
+});
+
+function feed_box() {
+    $.ajax({
+        type: 'GET',
+        url: '/main/feed',
+        date: {},
+        success: function (response) {
+            let rows = response['feeds']
+            for(let i = 0; i < rows.length; i++) {
+                let title =rows[i]['title']
+                let user_id = rows[i]['user_id']
+                let category = rows[i]['category']
+                let date = rows[i]['date']
+                let content = rows[i]['content']
+                let like = rows[i]['like']
+                let comment = rows[i]['comment']
+                let price = rows[i]['price']
+                let current = rows[i]['current']
+                let dead = rows[i]['dead']
+
+                let temp_html = `<div class="mainBox" style=" cursor: pointer;" onclick="location.href='#';">
+                                    <h2>${title}</h2>
+                                    <h5>ID: ${user_id}</h5>
+                                    <h6>작성일: ${date}</h6>
+                                    <h6>분류: ${category}</h6>
+                                    <div class="bodyBox">
+                                        <p>${content}
+                                        </p>
+                                    </div>
+                        
+                                    <button class="likeBox">
+                                        ❤ ${like}
+                                    </button>
+                        
+                                    <button class="commentBox">
+                                        댓글(${comment})
+                                    </button>
+                        
+                                    <div class="deadline">
+                                        마감: ${dead}
+                                    </div>
+                        
+                                    <div class="currentState">
+                                        상태: ${current}
+                                    </div>
+                        
+                                    <div class="price">
+                                        현재 입찰가격: ${price}원
+                                    </div>
+                                </div> </br>`
+                $('#allBox').append(temp_html)
+
+            }
+        }
+    });
+}
